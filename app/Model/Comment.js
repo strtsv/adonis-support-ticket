e9c8336b5402064a637a3a181836d99533e0aa48
jsusilo <enr@bk.ru>
2020-02-07 00:15:18 +0500

    comments

--- app/Model/Comment.js
+++ app/Model/Comment.js
@@ -3,7 +3,12 @@
 const Lucid = use('Lucid')
 
 class Comment extends Lucid {
-
+  ticket() {
+    return this.belongsTo('App/Model/Ticket')
+  }
+  user() {
+    return this.belongsTo('App/Model/User')
+  }
 }
 
 module.exports = Comment
