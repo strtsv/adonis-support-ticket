643930ccddf2351b0aa3827bb1289b655f3c051f
jsusilo <enr@bk.ru>
2020-02-11 22:54:59 +0500

    set up the store category

--- app/Http/Controllers/CategoriesController.js
+++ app/Http/Controllers/CategoriesController.js
@@ -1,15 +1,50 @@
 'use strict'
 
 const Category = use ('App/Model/Category')
+const Validator = use('Validator')
 
 class CategoriesController {
-  /**
-   * Display all tickets.
-   */
+
   * index(request, response) {
     const categories = yield Category.all()
 
     yield response.sendView('category.index', { categories: categories.toJSON() })
+  }
+
+  * add(request,response) {
+    yield response.sendView('category.add')
+  }
+
+  * store(request,response) {
+    const user = request.currentUser
+    const name = request.input('name')
+
+    // validate form input
+    const validation = yield Validator.validateAll(request.all(), {
+        name: 'required'
+
+    })
+
+    // show error messages upon validation fail
+    if (validation.fails()) {
+        yield request
+            .withAll()
+            .andWith({ errors: validation.messages() })
+            .flash()
+
+        return response.redirect('back')
+    }
+
+    // persist ticket to database
+    const category = yield Category.create({
+        name: request.input('name'),
+
+    })
+
+    console.log(`user is ${user.email} and name is ${name}`);
+
+    yield request.with({ status: `A ticket with ID: has been opened.` }).flash()
+    response.redirect('/category')
   }
 }
 
