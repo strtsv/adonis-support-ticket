b4604721bbf72c13b159eea239dcd894dd734815
jsusilo <enr@bk.ru>
2020-02-14 09:10:52 +0500

    set up Department

--- app/Http/Controllers/DepartmentController.js
+++ app/Http/Controllers/DepartmentController.js
@@ -1,6 +1,35 @@
-'use strict'
+"use strict";
+
+const Department = use("App/Model/Department");
+const Validator = use("Validator");
 
 class DepartmentController {
+  *index(response) {
+    departments = yield Department.all();
+    yield response.sendView("departemens.index", {
+      departemens: depart.toJSON()
+    });
+  }
+  *create(response) {
+    yield response.sendView("departemens.create");
+  }
+  *store(response) {
+    const validation = yield Validator.validateAll(request.all(), {
+      name: "required"
+    });
+    if (validation.fails()) {
+      yield request
+        .withAll()
+        .andWith({ errors: validation.messages() })
+        .flash();
+      return response.redirect("back");
+    }
+    const depart = yield departemens.create({
+      name: request.input("name")
+    });
+    yield request.with({ status: `Success create new Department.` }).flash();
+    response.redirect("/department");
+  }
 }
 
-module.exports = DepartmentController
+module.exports = DepartmentController;
