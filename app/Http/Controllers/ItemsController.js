75edb2772012967c571c1fa19d1f90e0390451cc
jsusilo <enr@bk.ru>
2020-02-23 23:47:28 +0500

    set up theh items methods

--- app/Http/Controllers/ItemsController.js
+++ app/Http/Controllers/ItemsController.js
@@ -14,6 +14,18 @@ class ItemsController {
   * create (request,response) {
     yield response.sendView('item.create')
   }
+  * show (request,response) {
+    const item = yield Items.query().where('id',request.param('item_id')).firstOrFail()
+    const time = Antl.formatDate(item.date_buy, { month:'numeric',day:'numeric',year:'numeric' })
+
+    yield response.sendView('admin.item.show',{items:item.toJSON(),time:time } )
+  }
+  * edit (request,response) {
+    const item = yield Items.query().where('id',request.param('item_id')).firstOrFail()
+    const time = Antl.formatDate(item.date_buy, { month:'numeric',day:'numeric',year:'numeric' })
+    yield response.sendView('admin.item.edit',{ items:item.toJSON(),time:time } )
+    console.log(time);
+  }
 }
 
 module.exports = ItemsController;
