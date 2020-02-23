d6837a9d6ff05f2015eaedd879debd52c993affa
jsusilo <enr@bk.ru>
2020-02-24 00:14:19 +0500

    set up theh items views

--- app/Http/Controllers/ItemsController.js
+++ app/Http/Controllers/ItemsController.js
@@ -1,6 +1,8 @@
 "use strict";
 const Items = use("App/Model/Item");
 const Category = use("App/Model/Category");
+const Validator = use("Validator");
+const Antl = use('Antl');
 
 class ItemsController {
   *index(request, response) {
@@ -11,20 +13,69 @@ class ItemsController {
       category: category.toJSON()
     });
   }
-  * create (request,response) {
-    yield response.sendView('item.create')
+  *create(request, response) {
+    yield response.sendView("items.create");
   }
-  * show (request,response) {
-    const item = yield Items.query().where('id',request.param('item_id')).firstOrFail()
-    const time = Antl.formatDate(item.date_buy, { month:'numeric',day:'numeric',year:'numeric' })
-
-    yield response.sendView('admin.item.show',{items:item.toJSON(),time:time } )
+  *show(request, response) {
+    const item = yield Items.query()
+      .where("id", request.param("item_id"))
+      .firstOrFail();
+    const time = Antl.formatDate(item.date_buy, {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric"
+    });
+    yield response.sendView("items.show", {
+      items: item.toJSON(),
+      time: time
+    });
   }
-  * edit (request,response) {
-    const item = yield Items.query().where('id',request.param('item_id')).firstOrFail()
-    const time = Antl.formatDate(item.date_buy, { month:'numeric',day:'numeric',year:'numeric' })
-    yield response.sendView('admin.item.edit',{ items:item.toJSON(),time:time } )
+  *edit(request, response) {
+    const item = yield Items.query()
+      .where("id", request.param("item_id"))
+      .firstOrFail();
+    const time = Antl.formatDate(item.date_buy, {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric"
+    });
+    yield response.sendView("items.edit", {
+      items: item.toJSON(),
+      time: time
+    });
     console.log(time);
+  }
+  *store(request, response) {
+    const user = request.currentUser;
+    const validation = yield Validator.validateAll(request.all(), {
+      code: "required",
+      name: "required",
+      desc: "required",
+      room: "required",
+      vendor: "required",
+      notes: "required",
+      datebuy: "required",
+      expired: "required"
+    });
+    const items = yield Items.create({
+      code: request.input("code"),
+      name: request.input("name"),
+      desc: request.input("desc"),
+      room: request.input("room"),
+      vendor: request.input("vendor"),
+      notes: request.input("notes"),
+      date_buy: request.input("datebuy"),
+      expired: request.input("expired")
+    });
+    if (validation.fails()) {
+      yield request
+        .withAll()
+        .andWith({ errors: validation.messages() })
+        .flash();
+      return response.redirect("back");
+    }
+    yield request.with({ status: `A Items is Created .` }).flash();
+    response.redirect("/items");
   }
 }
 
